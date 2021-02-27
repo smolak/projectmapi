c81b56a67a07c7aa6316b191015a9828f6ee1a6b
Jacek Smolak <jacek.smolak@gmail.com>
2021-02-27 17:54:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const fastify = require('fastify')()
+const drugsData = require('./drugs-data.json');
+
+fastify.decorate('notFound', (request, reply) => {
+  reply.code(404).type('text/html').send('Not Found')
+})
+
+fastify.setNotFoundHandler(fastify.notFound);
+
+const schema = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+          },
+          'package': {
+            type: 'string'
+          },
+          ean: {
+            type: 'number'
+          }
+        }
+      }
+    }
+  }
+}
+
+fastify.get('/:ean', schema, function (req, reply) {
+  const ean = req.params.ean;
+  const drugData = drugsData[ean];
+
+  if (!drugData) {
+    return fastify.notFound(req, reply);
+  }
+
+  reply.send({
+    name: drugData.name,
+    'package': drugData.package,
+    ean: drugData.ean
+  });
+})
+
+fastify.listen(3000)
