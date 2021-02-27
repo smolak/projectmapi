ce8a54b231da219b59da4a466d8afd9035c4e4b1
Jacek Smolak <jacek.smolak@gmail.com>
2021-02-27 18:14:53 +0100

    Look like Vercel has its own server implementation

--- api/index.js
+++ api/index.js
@@ -1,50 +1,69 @@
-'use strict'
-
-const fastify = require('fastify')()
 const drugsData = require('./drugs-data.json');
 
-fastify.decorate('notFound', (request, reply) => {
-  reply.code(404).type('text/html').send('Not Found')
-})
-
-fastify.setNotFoundHandler(fastify.notFound);
-
-const schema = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          name: {
-            type: 'string',
-          },
-          'package': {
-            type: 'string'
-          },
-          ean: {
-            type: 'number'
-          }
-        }
-      }
-    }
-  }
-}
-
-fastify.get('/:ean', schema, function (req, reply) {
-  const ean = req.params.ean;
+module.exports = (req, res) => {
+  const { ean } = req.query;
   const drugData = drugsData[ean];
 
   if (!drugData) {
-    return fastify.notFound(req, reply);
+    res.status(404).send('Not found');
   }
 
-  reply.send({
+  const data = {
     name: drugData.name,
     'package': drugData.package,
     ean: drugData.ean
-  });
-})
+  };
 
-fastify.listen(3000);
+  res.status(200).send(data);
+}
 
-console.log('Listening on 3000');
+// 'use strict'
+//
+// const fastify = require('fastify')()
+// const drugsData = require('./drugs-data.json');
+//
+// fastify.decorate('notFound', (request, reply) => {
+//   reply.code(404).type('text/html').send('Not Found')
+// })
+//
+// fastify.setNotFoundHandler(fastify.notFound);
+//
+// const schema = {
+//   schema: {
+//     response: {
+//       200: {
+//         type: 'object',
+//         properties: {
+//           name: {
+//             type: 'string',
+//           },
+//           'package': {
+//             type: 'string'
+//           },
+//           ean: {
+//             type: 'number'
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+//
+// fastify.get('/:ean', schema, function (req, reply) {
+//   const ean = req.params.ean;
+//   const drugData = drugsData[ean];
+//
+//   if (!drugData) {
+//     return fastify.notFound(req, reply);
+//   }
+//
+//   reply.send({
+//     name: drugData.name,
+//     'package': drugData.package,
+//     ean: drugData.ean
+//   });
+// })
+//
+// fastify.listen(3000);
+//
+// console.log('Listening on 3000');
